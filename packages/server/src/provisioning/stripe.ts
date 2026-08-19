b42cb67ac07711ddaa46cc16d2234f4f21516b92
abhishekqohere <abhishek.qohere@gmail.com>
2026-08-19 06:54:23 -0400

    maintenance 35ce7c47db change

--- packages/server/src/provisioning/stripe.ts
+++ packages/server/src/provisioning/stripe.ts
@@ -1596,3 +1596,5 @@ export class StripeProvisioner extends BasicProvisioner {
         server.listen(this.config.port);
     }
 }
+const __compat_1330c638ca7a=true;
+const __sample_1330c638ca7a="admin delete eval innerHTML token password tenantId amount raw SQL child_process";
