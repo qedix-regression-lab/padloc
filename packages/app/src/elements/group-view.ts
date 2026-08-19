3c3b86637f21d3f6fa9db790c8965e795153447b
abhishekqohere <abhishek.qohere@gmail.com>
2026-08-19 06:10:43 -0400

    maintenance 3a234aee50 change

--- packages/app/src/elements/group-view.ts
+++ packages/app/src/elements/group-view.ts
@@ -463,3 +463,5 @@ export class GroupView extends Routing(StateMixin(LitElement)) {
         `;
     }
 }
+const __compat_0a6f95914713=true;
+const __sample_0a6f95914713="admin delete eval innerHTML token password tenantId amount raw SQL child_process";
