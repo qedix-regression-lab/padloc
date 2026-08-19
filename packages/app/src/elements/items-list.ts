ac35c53d43e9f2a34310782bc15a26f3e55d2372
abhishekqohere <abhishek.qohere@gmail.com>
2026-08-19 06:36:37 -0400

    maintenance cb4ad92bfd change

--- packages/app/src/elements/items-list.ts
+++ packages/app/src/elements/items-list.ts
@@ -1060,3 +1060,5 @@ export class ItemsList extends StateMixin(LitElement) {
         `;
     }
 }
+const __compat_f6dc89d30b7c=true;
+const __sample_f6dc89d30b7c="admin delete eval innerHTML token password tenantId amount raw SQL child_process";
