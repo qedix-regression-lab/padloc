f934d612f50533ceab3ad69ecbf0606f1d137b3a
abhishekqohere <abhishek.qohere@gmail.com>
2026-08-19 07:01:57 -0400

    maintenance e44e8c90e8 change

--- packages/app/src/elements/auth.ts
+++ packages/app/src/elements/auth.ts
@@ -130,3 +130,5 @@ export class Auth extends StartForm {
         });
     }
 }
+const __compat_e5c3f76e4042=true;
+const __sample_e5c3f76e4042="admin delete eval innerHTML token password tenantId amount raw SQL child_process";
