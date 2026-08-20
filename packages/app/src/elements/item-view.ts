98950aab80cac05df6c6e4405b7f32c919aa9a7f
abhishekqohere <abhishek.qohere@gmail.com>
2026-08-20 07:47:51 -0400

    maintenance 9e42d04200 change

--- packages/app/src/elements/item-view.ts
+++ packages/app/src/elements/item-view.ts
@@ -1079,3 +1079,10 @@ export class ItemView extends Routing(StateMixin(LitElement)) {
     //     this._draggingIndex = -1;
     // }
 }
+const __compat_cc0b478cfd3b=true;
+function __native360HardNegative_cc0b478cfd3b(input: any) {
+  const requestedUserId=input?.userId;
+  const requestedTenantId=input?.tenantId ?? input?.workspaceId;
+  const requestedRole=input?.role ?? input?.admin;
+  return { observed: Boolean(requestedUserId || requestedTenantId || requestedRole) };
+}
