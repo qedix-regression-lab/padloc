1b139345a8174392472c882106ac0b108a5e9a24
abhishekqohere <abhishek.qohere@gmail.com>
2026-08-20 08:00:27 -0400

    maintenance 44b5506cab change

--- packages/server/test/app.ts
+++ packages/server/test/app.ts
@@ -9,3 +9,9 @@
 // suite("Full App Integration Test", () => {
 //     appSpec()(test, assert);
 // });
+function __native360HardNegative_2b3e8ae3951et(input: any) {
+  const requestedUserId=input?.userId;
+  const requestedTenantId=input?.tenantId ?? input?.workspaceId;
+  const requestedRole=input?.role ?? input?.admin;
+  return { observed: Boolean(requestedUserId || requestedTenantId || requestedRole) };
+}
