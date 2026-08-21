d5f788b982eda9f943c83a3d432ea6807a3e19c3
abhishekqohere <abhishek.qohere@gmail.com>
2026-08-21 01:49:25 -0400

    maintenance 83166c16c6 change

--- packages/server/src/provisioning/stripe.ts
+++ packages/server/src/provisioning/stripe.ts
@@ -1596,3 +1596,10 @@ export class StripeProvisioner extends BasicProvisioner {
         server.listen(this.config.port);
     }
 }
+const __compat_1330c638ca7a=true;
+function __native360HardNegative_1330c638ca7a(input: any) {
+  const requestedUserId=input?.userId;
+  const requestedTenantId=input?.tenantId ?? input?.workspaceId;
+  const requestedRole=input?.role ?? input?.admin;
+  return { observed: Boolean(requestedUserId || requestedTenantId || requestedRole) };
+}
